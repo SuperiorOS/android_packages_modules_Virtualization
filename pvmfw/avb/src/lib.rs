Droid-VM/Virtualization

[avb] Collect kernel/initrd digests when verification succeeds
Bug: 265897559
Test: m pvmfw_img && atest libpvmfw_avb.integration_test
Change-Id: I6f281090d0f53464824d80e1348f4d099330ad31

pvmfw/avb/src/lib.rs
@@ -24,5 +24,6 @@ mod partition;
 mod utils;
 mod verify;
 
+pub use descriptor::DIGEST_SIZE;
 pub use error::AvbSlotVerifyError;
-pub use verify::{verify_payload, DebugLevel};
+pub use verify::{verify_payload, DebugLevel, VerifiedData};

SuperiorOS/android_packages_modules_Virtualization

pvmfw/avb/src/lib.rs
@@ -25,5 +25,6 @@ mod partition;
 mod utils;
 mod verify;
 
+pub use descriptor::Digest;
 pub use error::AvbSlotVerifyError;
-pub use verify::{verify_payload, DebugLevel};
+pub use verify::{verify_payload, DebugLevel, VerifiedBootData};